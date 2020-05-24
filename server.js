a5a6dbe712328fb0a3be766bb44b3931320619f0
Bruno Nascimento <brunonascimento@MacBook-Pro-de-Bruno.local>
2020-05-23 21:48:35 -0300

    Added graphql

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 const multer = require('multer');
+const graphqlHttp = require('express-graphql');
 
-const feedRoutes = require('./src/routes/feedRoutes');
-const authRoutes = require('./src/routes/authRoutes');
+const bodyParser = require('body-parser');
+
+const graphqlSchema = require('./src/graphql/schema');
+const graphqlResolver = require('./src/graphql/resolvers');
+const auth = require('./src/middleware/is-auth');
+const { clearImage } = require('./src/util/file');
 
 const app = express();
 
@@ -30,17 +35,60 @@ const fileFilter = (req, file, cb) => {
   }
 };
 
-app.use(cors());
-
-app.use(express.json());
-
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+app.use(auth);
+
+app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+  return res
+    .status(201)
+    .json({ message: 'File stored.', filePath: req.file.path });
+});
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphqlSchema,
+    rootValue: graphqlResolver,
+    graphiql: true,
+    formatError(err) {
+      if (!err.originalError) {
+        return err;
+      }
+      const data = err.originalError.data;
+      const message = err.message || 'An error occurred.';
+      const code = err.originalError.code || 500;
+      return { message: message, status: code, data: data };
+    }
+  })
+);
 
 app.use((error, req, res, next) => {
   console.log(error);
